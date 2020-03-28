6323948969c7b37ea6fc9fd97086aee6f2717c13
jaycosaur <jaycorichter@gmail.com>
2020-03-28 21:58:13 +1100

    chore: bump version for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="pyring",
-    version="0.0.10",
+    version="0.0.11",
     author="Jacob Richter",
     author_email="jaycorichter@gmail.com",
     description="A ring buffer implemented in pure python.",
